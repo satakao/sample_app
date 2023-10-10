24f8e9128554b22f064eccc6c63877d87a5d0836
TakaoSatou <142711130+satakao@users.noreply.github.com>
2023-10-10 08:13:27 +0000

    [finish]3-11

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,10 +4,13 @@ class ListsController < ApplicationController
   end
   
   def create
-  list = List.new(list_params)
-  list.save
-  redirect_to list_path(list.id)
-  
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      @lists = List.all
+      render :index
+    end
   end
   def index
     @lists = List.all
@@ -20,9 +23,22 @@ class ListsController < ApplicationController
   end
 
   def edit
+    @list = List.find(params[:id])
+  end
+  
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+    
+  end
+  def destroy
+    list = List.find(params[:id])  # データ（レコード）を1件取得
+    list.destroy  # データ（レコード）を削除
+    redirect_to '/lists'  # 投稿一覧画面へリダイレクト
   end
   private
   def list_params
-    params.require(:list).permit(:title,:body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
